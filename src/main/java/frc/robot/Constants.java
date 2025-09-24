4660ab512c6d8b8e20679ef3ce721b36662f6fc5
zach <zacharaya@gmail.com>
2025-09-24 11:14:55 -0700

    created visual apriltag map for the reef

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import java.util.Map;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -68,6 +74,39 @@ public final class Constants {
   public static final class LedConstants {
     public static final int LED_PORT = 5;
     public static final int LED_LENGTH = 142;
+  }
+
+  public static final class FieldConstants {
+    // Official AprilTag locations from the 2025 Game Manual (User Guide v1.1)
+    // The format is (X, Y, Z) in meters and rotation in radians.
+    // All measurements are relative to the Blue Alliance origin.
+    public static final Map<Integer, Pose3d> APRIL_TAG_FIELD_LAYOUT = Map.ofEntries(
+        // --- RED ALLIANCE TAGS (IDs 1-11) ---
+        Map.entry(1, new Pose3d(new Translation3d(Units.inchesToMeters(610.77), Units.inchesToMeters(42.19), Units.inchesToMeters(18.22)), new Rotation3d(0, 0, Units.degreesToRadians(120)))),
+        Map.entry(2, new Pose3d(new Translation3d(Units.inchesToMeters(637.96), Units.inchesToMeters(118.19), Units.inchesToMeters(27.38)), new Rotation3d(0, 0, Units.degreesToRadians(180)))),
+        Map.entry(3, new Pose3d(new Translation3d(Units.inchesToMeters(637.96), Units.inchesToMeters(196.19), Units.inchesToMeters(27.38)), new Rotation3d(0, 0, Units.degreesToRadians(180)))),
+        Map.entry(4, new Pose3d(new Translation3d(Units.inchesToMeters(610.77), Units.inchesToMeters(272.19), Units.inchesToMeters(18.22)), new Rotation3d(0, 0, Units.degreesToRadians(240)))),
+        Map.entry(5, new Pose3d(new Translation3d(Units.inchesToMeters(39.23), Units.inchesToMeters(42.19), Units.inchesToMeters(18.22)), new Rotation3d(0, 0, Units.degreesToRadians(60)))),
+        Map.entry(6, new Pose3d(new Translation3d(Units.inchesToMeters(12.04), Units.inchesToMeters(118.19), Units.inchesToMeters(27.38)), new Rotation3d(0, 0, 0))),
+        Map.entry(7, new Pose3d(new Translation3d(Units.inchesToMeters(12.04), Units.inchesToMeters(196.19), Units.inchesToMeters(27.38)), new Rotation3d(0, 0, 0))),
+        Map.entry(8, new Pose3d(new Translation3d(Units.inchesToMeters(39.23), Units.inchesToMeters(272.19), Units.inchesToMeters(18.22)), new Rotation3d(0, 0, Units.degreesToRadians(300)))),
+        Map.entry(9, new Pose3d(new Translation3d(Units.inchesToMeters(535.06), Units.inchesToMeters(295.25), Units.inchesToMeters(18.25)), new Rotation3d(0, 0, 0))),
+        Map.entry(10, new Pose3d(new Translation3d(Units.inchesToMeters(578.06), Units.inchesToMeters(216.75), Units.inchesToMeters(18.25)), new Rotation3d(0, 0, 0))),
+        Map.entry(11, new Pose3d(new Translation3d(Units.inchesToMeters(535.06), Units.inchesToMeters(138.25), Units.inchesToMeters(18.25)), new Rotation3d(0, 0, 0))),
+
+        // --- BLUE ALLIANCE TAGS (IDs 12-22) ---
+        Map.entry(12, new Pose3d(new Translation3d(Units.inchesToMeters(636.5), Units.inchesToMeters(138.25), Units.inchesToMeters(24.0)), new Rotation3d(0, 0, Units.degreesToRadians(60)))),
+        Map.entry(13, new Pose3d(new Translation3d(Units.inchesToMeters(114.94), Units.inchesToMeters(295.25), Units.inchesToMeters(18.25)), new Rotation3d(0, 0, Units.degreesToRadians(180)))),
+        Map.entry(14, new Pose3d(new Translation3d(Units.inchesToMeters(71.94), Units.inchesToMeters(216.75), Units.inchesToMeters(18.25)), new Rotation3d(0, 0, Units.degreesToRadians(180)))),
+        Map.entry(15, new Pose3d(new Translation3d(Units.inchesToMeters(114.94), Units.inchesToMeters(138.25), Units.inchesToMeters(18.25)), new Rotation3d(0, 0, Units.degreesToRadians(180)))),
+        Map.entry(16, new Pose3d(new Translation3d(Units.inchesToMeters(13.5), Units.inchesToMeters(138.25), Units.inchesToMeters(24.0)), new Rotation3d(0, 0, Units.degreesToRadians(120)))),
+        Map.entry(17, new Pose3d(new Translation3d(Units.inchesToMeters(405.25), Units.inchesToMeters(157.25), Units.inchesToMeters(34.38)), new Rotation3d(0, 0, Units.degreesToRadians(180)))),
+        Map.entry(18, new Pose3d(new Translation3d(Units.inchesToMeters(327.00), Units.inchesToMeters(304.25), Units.inchesToMeters(24.0)), new Rotation3d(0, 0, Units.degreesToRadians(90)))),
+        Map.entry(19, new Pose3d(new Translation3d(Units.inchesToMeters(327.00), Units.inchesToMeters(10.25), Units.inchesToMeters(24.0)), new Rotation3d(0, 0, Units.degreesToRadians(270)))),
+        Map.entry(20, new Pose3d(new Translation3d(Units.inchesToMeters(244.75), Units.inchesToMeters(157.25), Units.inchesToMeters(34.38)), new Rotation3d(0, 0, 0))),
+        Map.entry(21, new Pose3d(new Translation3d(Units.inchesToMeters(323.00), Units.inchesToMeters(304.25), Units.inchesToMeters(24.0)), new Rotation3d(0, 0, Units.degreesToRadians(90)))),
+        Map.entry(22, new Pose3d(new Translation3d(Units.inchesToMeters(323.00), Units.inchesToMeters(10.25), Units.inchesToMeters(24.0)), new Rotation3d(0, 0, Units.degreesToRadians(270))))
+    );
   }
 }
 
--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,5 +1,4 @@
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
